7393843720f38e92bfcd07d299fcba5fc9af559f
Erik <bergsc@kth.se>
2012-08-23 14:49:37 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width" />
+
+	<title>Compass testing</title>
+  
+	<!-- Included CSS Files -->
+	<link rel="stylesheet" href="stylesheets/screen.css">
+	<link rel="stylesheet" href="stylesheets/styles.css">
+
+</head>
+<body>
+	<!-- container -->
+	<div class="container">
+		<header>Header</header>
+		<div class="sidebar-first">
+			Urna eu dolor, pellentesque, egestas, cras mattis enim lacus a? Penatibus? Et? Hac facilisis in? Mid nunc, est, eros. Nec augue? Risus tincidunt hac sit augue mid montes. Pid egestas, scelerisque proin tortor auctor porta amet! Parturient sed natoque et enim porta magnis ac tempor! Nascetur egestas natoque diam, augue, integer habitasse natoque ultrices, egestas risus integer ac mid! Tortor in, integer sit, massa, lectus phasellus turpis aliquam. Rhoncus ut adipiscing dignissim, augue augue pid tempor. Sed ultrices, parturient sed montes et scelerisque montes, elementum urna augue platea adipiscing lectus? Vel ut! Cras cum nec augue? Sed et est mus montes porta. Integer nisi enim, enim vut et penatibus tortor auctor magna duis est, ut tempor porta nec rhoncus ac.
+
+			Lacus augue vel mattis phasellus, vel rhoncus, sociis. Rhoncus vel pid, sociis ridiculus aenean massa lorem risus nunc mauris. Cursus integer tempor turpis, adipiscing nunc lundium ut? Placerat! Nisi. Nisi sit eu, sed, lacus cum! In, turpis hac ac, sagittis nec, rhoncus odio penatibus ultricies lundium turpis, est ut sed turpis. Pellentesque placerat auctor a elit parturient a et! Sociis porttitor vel eu vel lorem elementum egestas hac integer, ridiculus turpis? Pellentesque magnis augue cras? Augue ac! Adipiscing lectus! Parturient ultricies magna a habitasse pid hac auctor. Phasellus in, porta porta nec quis adipiscing mauris? Urna et, platea odio, tortor proin rhoncus pulvinar scelerisque porttitor amet magnis et pulvinar etiam nunc cras aliquet! Vel massa sociis eu enim lundium.
+		</div>
+		<div class="main">
+			Urna eu dolor, pellentesque, egestas, cras mattis enim lacus a? Penatibus? Et? Hac facilisis in? Mid nunc, est, eros. Nec augue? Risus tincidunt hac sit augue mid montes. Pid egestas, scelerisque proin tortor auctor porta amet! Parturient sed natoque et enim porta magnis ac tempor! Nascetur egestas natoque diam, augue, integer habitasse natoque ultrices, egestas risus integer ac mid! Tortor in, integer sit, massa, lectus phasellus turpis aliquam. Rhoncus ut adipiscing dignissim, augue augue pid tempor. Sed ultrices, parturient sed montes et scelerisque montes, elementum urna augue platea adipiscing lectus? Vel ut! Cras cum nec augue? Sed et est mus montes porta. Integer nisi enim, enim vut et penatibus tortor auctor magna duis est, ut tempor porta nec rhoncus ac.
+
+			Lacus augue vel mattis phasellus, vel rhoncus, sociis. Rhoncus vel pid, sociis ridiculus aenean massa lorem risus nunc mauris. Cursus integer tempor turpis, adipiscing nunc lundium ut? Placerat! Nisi. Nisi sit eu, sed, lacus cum! In, turpis hac ac, sagittis nec, rhoncus odio penatibus ultricies lundium turpis, est ut sed turpis. Pellentesque placerat auctor a elit parturient a et! Sociis porttitor vel eu vel lorem elementum egestas hac integer, ridiculus turpis? Pellentesque magnis augue cras? Augue ac! Adipiscing lectus! Parturient ultricies magna a habitasse pid hac auctor. Phasellus in, porta porta nec quis adipiscing mauris? Urna et, platea odio, tortor proin rhoncus pulvinar scelerisque porttitor amet magnis et pulvinar etiam nunc cras aliquet! Vel massa sociis eu enim lundium.
+			<div id="paper">
+			</div>
+		</div>
+		<div class="sidebar-second">
+			Urna eu dolor, pellentesque, egestas, cras mattis enim lacus a? Penatibus? Et? Hac facilisis in? Mid nunc, est, eros. Nec augue? Risus tincidunt hac sit augue mid montes. Pid egestas, scelerisque proin tortor auctor porta amet! Parturient sed natoque et enim porta magnis ac tempor! Nascetur egestas natoque diam, augue, integer habitasse natoque ultrices, egestas risus integer ac mid! Tortor in, integer sit, massa, lectus phasellus turpis aliquam. Rhoncus ut adipiscing dignissim, augue augue pid tempor. Sed ultrices, parturient sed montes et scelerisque montes, elementum urna augue platea adipiscing lectus? Vel ut! Cras cum nec augue? Sed et est mus montes porta. Integer nisi enim, enim vut et penatibus tortor auctor magna duis est, ut tempor porta nec rhoncus ac.
+
+			Lacus augue vel mattis phasellus, vel rhoncus, sociis. Rhoncus vel pid, sociis ridiculus aenean massa lorem risus nunc mauris. Cursus integer tempor turpis, adipiscing nunc lundium ut? Placerat! Nisi. Nisi sit eu, sed, lacus cum! In, turpis hac ac, sagittis nec, rhoncus odio penatibus ultricies lundium turpis, est ut sed turpis. Pellentesque placerat auctor a elit parturient a et! Sociis porttitor vel eu vel lorem elementum egestas hac integer, ridiculus turpis? Pellentesque magnis augue cras? Augue ac! Adipiscing lectus! Parturient ultricies magna a habitasse pid hac auctor. Phasellus in, porta porta nec quis adipiscing mauris? Urna et, platea odio, tortor proin rhoncus pulvinar scelerisque porttitor amet magnis et pulvinar etiam nunc cras aliquet! Vel massa sociis eu enim lundium.
+		</div>
+		<footer>Footer</footer>
+	</div>
+	
+	<script src="javascripts/jquery-1.7.2.min.js"></script>
+	<script src="javascripts/raphael-min.js"></script>
+	<script src="javascripts/scripts.js"></script>
+	
+</body>
+</html>
---
+++ javascripts/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	/*
+	paper = new Raphael("paper","500", "500");
+	paper.rect(50, 50, 200, 200, 10).attr({fill: "#39C"});
+	*/
+	
+});
